3944e5e8ecdab4b5d2b3d3701bbcaa298df52a5d
Rohit Singh <conankmails@gmail.com>
2020-01-03 14:05:55 +0530

    Patterns folder added

---
+++ Patterns/pat5.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(char i=65;i<=69;i++)
+    {
+        for(char j=65;j<=69;j++)
+        {
+            cout<<j<<" ";
+        }cout<<"\n";
+    }
+}
---
+++ Patterns/pat9.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(char i=69;i>=65;i--)
+    {
+        for(char j=69;j>=65;j--)
+        {
+            cout<<j<<" ";
+        }cout<<"\n";
+    }
+}
